15fb729f9aa0816376a65a7d1d439a002b122764
Borislav Vezirov <bobi.vez@gmail.com>
2022-04-16 17:59:29 +0300

    minor changes

--- src/components/Catalog.js
+++ src/components/Catalog.js
@@ -7,14 +7,11 @@ function Catalog() {
 
     let [cars, setCars] = useState([]);
     // let [loading, setLoading] = useState(false);
-
-    console.log(cars);
  
     useEffect(() => {
         // setLoading = true;
         getAllCars()
             .then(result => {
-                console.log(result);
                 // setLoading = false;
                 setCars(result);
             })
@@ -23,10 +20,10 @@ function Catalog() {
     return (
         <section id="car-listings">
             <h1>Car Listings</h1>
-            <div class="listings">
+            <div className="listings">
                 { cars.length > 0
                         ? cars.map(x => < CarCard key={x._id} car={x} />)
-                        : <p class="no-cars">No cars in database.</p>
+                        : <p className="no-cars">No cars in database.</p>
                 }
             </div>
         </section>
